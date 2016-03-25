680b299e3e1ac0d0e8d81ac8b4068f0e780b87c8
Lambros Petrou <lambrospetrou@gmail.com>
2016-03-25 20:05:04 +0000

    Refactored chronopaper. Added eslint ES6 with Airbnb styleguide.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // the new way to import modules
 import gulp from 'gulp';
 import sass from 'gulp-sass';
@@ -7,121 +5,125 @@ import autoprefixer from 'gulp-autoprefixer';
 import cssnano from 'gulp-cssnano';
 import sourcemaps from 'gulp-sourcemaps';
 
-// This plugin can be used to automatically load the plugins without importing them explicitly!!! ($.size)
+// This plugin can be used to automatically load the plugins without importing them explicitly!
 import gulpLoadPlugins from 'gulp-load-plugins';
 const $ = gulpLoadPlugins();
 
 // the old way to import modules
-var babel = require("gulp-babel");
-var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+import babel from 'gulp-babel';
+import clean from 'gulp-clean';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
 
 const dirs = {
   src: 'src/',
-  dist: 'build/'
+  dist: 'build/',
 };
 
 const paths = {
   html: ['index.html'],
   scripts: ['scripts/**/*.js', '!scripts/libs/**/*.js'],
-  libs: [], // 3rd-party JS
+  libs: ['scripts/libs/**/*.js'], // 3rd-party JS
   styles: ['styles/**/*.css', 'styles/**/*.scss'],
-  extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'] // other static files to be copied to the dist/
+  extras: [
+    'crossdomain.xml', 'humans.txt', 'manifest.appcache',
+    'robots.txt', 'favicon.ico',
+  ], // other static files to be copied to the dist/
 };
 
 function createPath(dir, pathArray) {
-  return pathArray.map( (path) => { return dir + path; } );
+  return pathArray.map((path) => dir + path);
 }
 
 // Delete the dist directory
-gulp.task('clean', function() {
-  return gulp
+gulp.task('clean', () => {
+  gulp
     .src(dirs.dist)
     .pipe(clean());
 });
 
 /** SASS **/
 const autoprefixerOptions = {
-  browsers: []
+  browsers: [],
 };
-const sassOptions = { 
-  outputStyle: 'compressed', 
-  errLogToConsole: true 
+const sassOptions = {
+  outputStyle: 'compressed',
+  errLogToConsole: true,
 };
 gulp.task('sass', () => {
-  return gulp
-    .src(paths.styles, {cwd: dirs.src})
+  gulp
+    .src(paths.styles, { cwd: dirs.src })
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(concat('styles.min.css'))
     .pipe(cssnano())
-    .pipe($.size({title: 'styles'}))
+    .pipe($.size({ title: 'styles' }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dirs.dist + 'styles/'));
+    .pipe(gulp.dest(`${dirs.dist}styles/`));
 });
-gulp.task('sass-prod', () => {
-  return gulp
-    .src(paths.styles, {cwd: dirs.src})
+gulp.task('sass:prod', () => {
+  gulp
+    .src(paths.styles, { cwd: dirs.src })
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(concat('styles.min.css'))
     .pipe(cssnano())
-    .pipe($.size({title: 'styles'}))
-    .pipe(gulp.dest(dirs.dist + 'styles/'));
+    .pipe($.size({ title: 'styles' }))
+    .pipe(gulp.dest(`${dirs.dist}styles/`));
 });
 
 /** SCRIPTS **/
 // Process scripts and concatenate them into one output file
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   gulp
-    .src(paths.scripts, {cwd: dirs.src})
+    .src(paths.scripts, { cwd: dirs.src })
     .pipe(babel())
     .pipe(concat('app.min.js'))
     .pipe(uglify())
-    .pipe($.size({title: 'scripts'}))
-    .pipe(gulp.dest(dirs.dist + 'scripts/'));
+    .pipe($.size({ title: 'scripts' }))
+    .pipe(gulp.dest(`${dirs.dist}scripts/`));
 });
 
 // Copy all other files to dist directly
-gulp.task('copy', function() {
+gulp.task('copy', () => {
   // Copy html
   gulp
-    .src(paths.html, {cwd: dirs.src})
+    .src(paths.html, { cwd: dirs.src })
     .pipe(gulp.dest(dirs.dist));
 
   // Copy extra files
   gulp
-    .src(paths.extras, {cwd: dirs.src})
+    .src(paths.extras, { cwd: dirs.src })
     .pipe(gulp.dest(dirs.dist));
 });
 
 /** WATCHER **/
-gulp.task('watch', function() {
+gulp.task('watch', () => {
+  function eventHandler(event) {
+    console.log(`File ${event.path} was ${event.type}, running tasks...`);
+  }
   gulp
     .watch(createPath(dirs.src, paths.styles), ['sass'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+    .on('change', eventHandler);
 
   gulp
     .watch(createPath(dirs.src, paths.scripts), ['scripts'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+    .on('change', eventHandler);
 
   gulp
     .watch(createPath(dirs.src, paths.html), ['copy'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+    .on('change', eventHandler);
+
+  gulp
+    .watch(createPath(dirs.src, paths.extras), ['copy'])
+    .on('change', eventHandler);
 });
 
 /** Task to change scripts used in the HTML. **/
 // http://tylermcginnis.com/reactjs-tutorial-pt-2-building-react-applications-with-gulp-and-browserify/
 
 gulp.task('dev', ['sass', 'scripts', 'copy']);
-gulp.task('release', ['sass-prod', 'scripts', 'copy']);
+gulp.task('release', ['sass:prod', 'scripts', 'copy']);
 
 gulp.task('default', ['dev']);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,116 +1,118 @@
 (function() {
 
-    var PaperBox = React.createClass({
-      loadPapersFromAPI: function() {
-        return [];
-      },
+  var containerElement = document.querySelector('#chronopaper');
 
-      getInitialState: function() {
-        return { 
-          data: [] 
-        };
-      },
+  var PaperForm = React.createClass({
 
-      componentDidMount: function() {
-        this.loadPapersFromAPI();
-      },
+    getInitialState: function() {
+      return {
+        text: ''
+      };
+    },
 
-      handleOnPaperSubmit: function(paper) {
-        this.setState({data: this.state.data.concat([paper])});
+    componentDidMount: function() {
+      containerElement.querySelector('.paper-text').focus();
+    },
+
+    handleTextChange: function(e) {
+      this.setState({ text: e.target.value });
+    },
+
+    handleSubmit: function(e) {
+      e.preventDefault();
+      var text = this.state.text.trim();
+      if (!text) {
+        return;
+      }
+
+      // Notify for paper submit
+      if (this.props.onPaperSubmit) {
+        // TODO create a hash ID for the paper
+        this.props.onPaperSubmit({text: text, id: text});
+      }
+      // Reset the paper form.
+      this.setState({text: ''});
+
+      containerElement.querySelector('.paper-text').focus();
+    },
+
+    render: function() {
+      return (
+        <div className="paper-form-container">
+          <form className="paper-form" onSubmit={this.handleSubmit}>
+            <textarea className="paper-text" placeholder="Start typing..." 
+              value={this.state.text} 
+              onChange={this.handleTextChange}></textarea>
+            <button type="submit">Post</button>
+          </form>
+        </div>
+      );
+    }
+  });
+
+  var Paper = React.createClass({
+    rawMarkup: function() {
+      var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+      return { __html: rawMarkup };
+    },
+
+    render: function() {
+      return (
+        <div className="paper">
+          <h2 className="paper-title">Untitled paper</h2>
+          <div dangerouslySetInnerHTML={this.rawMarkup()} />
+        </div>
+      );
+    }
+  });
+
+  var PaperList = React.createClass({
+    render: function() {
+      var paperNodes = this.props.data.map(function(paper) {
+        return (
+          <Paper title={paper.title} key={paper.id}>{paper.text}</Paper>
+        );
+      });
+      return (
+        <div className="paperList">{paperNodes}</div>
+      );
+    }
+  });
+
+  var PaperBox = React.createClass({
+    loadPapersFromAPI: function() {
+      return [];
+    },
+
+    getInitialState: function() {
+      return { 
+        data: [] 
+      };
+    },
+
+    componentDidMount: function() {
+      this.loadPapersFromAPI();
+    },
+
+    handleOnPaperSubmit: function(paper) {
+      this.setState({data: this.state.data.concat([paper])});
         // TODO send to the server and update the data[]
-        console.log(paper);
-      },
+      console.log(paper);
+    },
 
-      render: function() {
-        return (
-          <div className="papersBox">
-            <h1>Papers</h1>
-            <PaperList data={this.state.data} />
-            <PaperForm onPaperSubmit={this.handleOnPaperSubmit} />
-          </div>
-        );
-      }
-    });
+    render: function() {
+      return (
+        <div className="paper-box">
+          <h1>Papers</h1>
+          <PaperList data={this.state.data} />
+          <PaperForm onPaperSubmit={this.handleOnPaperSubmit} />
+        </div>
+      );
+    }
+  });
 
-    var PaperList = React.createClass({
-      render: function() {
-        var paperNodes = this.props.data.map(function(paper) {
-          return (
-            <Paper title={paper.title} key={paper.id}>{paper.text}</Paper>
-          );
-        });
-        return (
-          <div className="paperList">
-            {paperNodes}
-          </div>
-        );
-      }
-    });
-
-    var Paper = React.createClass({
-
-      rawMarkup: function() {
-        var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-        return { __html: rawMarkup };
-      },
-
-      render: function() {
-        return (
-          <div className="paper">
-            <h2 className="paperTitle">
-              {this.props.title}
-            </h2>
-            <div dangerouslySetInnerHTML={this.rawMarkup()} />
-          </div>
-        );
-      }
-    });
-
-    var PaperForm = React.createClass({
-
-      getInitialState: function() {
-        return {
-          title: '', text: ''
-        };
-      },
-
-      handleTitleChange: function(e) {
-        this.setState({ title: e.target.value });
-      },
-      handleTextChange: function(e) {
-        this.setState({ text: e.target.value });
-      },
-      handleSubmit: function(e) {
-        e.preventDefault();
-        var title = this.state.title.trim();
-        var text = this.state.text.trim();
-        if (!text || !title) {
-          return;
-        }
-
-        // Notify for paper submit
-        if (this.props.onPaperSubmit) {
-          this.props.onPaperSubmit({title: title, text: text});
-        }
-        // Reset the paper form.
-        this.setState({title: '', text: ''});
-      },
-
-      render: function() {
-        return (
-          <div className="paperForm">
-            <form className="paperForm-form" onSubmit={this.handleSubmit}>
-              <input type="text" placeholder="Paper Title" value={this.state.title} onChange={this.handleTitleChange} />
-              <textarea placeholder="Start typing..." value={this.state.text} onChange={this.handleTextChange}></textarea>
-              <button type="submit">Post</button>
-            </form>
-          </div>
-        );
-      }
-    });
-
-    ReactDOM.render(
-      <PaperBox url='/api/comments' />,
-      document.getElementById('chronopaper')
-    );	
+  ReactDOM.render(
+    <PaperBox />,
+    containerElement
+  );
 }).call(this);
