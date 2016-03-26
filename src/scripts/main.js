fde25f0a8c6c6a2cc15cd5d3580f44f710f4b46f
Lambros Petrou <lambrospetrou@gmail.com>
2016-03-26 12:46:39 +0000

    Basic functionality to edit paper notes implemented.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 (function() {
 
+  function buildPaperModel() {
+    var now = moment();
+    return {
+      text: '',
+      id: now.toISOString(),
+      dateUpdated: now,
+      dateCreated: now
+    };
+  } 
+
   var containerElement = document.querySelector('#chronopaper');
 
   function setState(_this, params) {
@@ -11,8 +21,7 @@
 
     getInitialState: function() {
       return {
-        view: 'editor',
-        text: '',
+        view: 'editor'
       };
     },
 
@@ -26,34 +35,31 @@
       });
     },
 
-    handleTextChange: function(e) {
-      setState(this, {
-        text: e.target.value
-      });
-    },
+    handlePaperTextChange: function(e) {
+      var text = e.target.value;
 
-    handleSubmit: function(e) {
-      e.preventDefault();
-      var text = this.state.text.trim();
-      if (!text) {
-        return;
-      }
+      // update the paper
+      var paper = Object.assign({}, this.props.paper, {
+        text: text,
+        dateUpdated: moment()
+      });
 
       // Notify for paper submit
       if (this.props.onPaperSubmit) {
         // TODO create a hash ID for the paper
-        this.props.onPaperSubmit({text: text, id: text});
+        this.props.onPaperSubmit(paper);
       }
-      // Reset the paper form.
-      setState(this, {
-        text: ''
-      });
+    },
 
-      containerElement.querySelector('.paper-text').focus();
+    handleNewPaper: function(e) {
+      e.preventDefault();
+      if (typeof this.props.onCreateNewPaper === 'function') {
+        this.props.onCreateNewPaper();
+      }
     },
 
     rawMarkup: function() {
-      var rawMarkup = marked(this.state.text, {sanitize: true});
+      var rawMarkup = marked(this.props.paper.text, {sanitize: true});
       return { __html: rawMarkup };
     },
 
@@ -63,9 +69,9 @@
         editorView = (
           <div>
             <textarea className="paper-text" placeholder="Start typing..." 
-              value={this.state.text} 
-              onChange={this.handleTextChange}></textarea>
-            <button type="submit">Post</button>
+              value={this.props.paper.text} 
+              onChange={this.handlePaperTextChange}></textarea>
+            <button type="submit">Create New</button>
           </div>
         );
       } else if (this.state.view === 'markdown') {
@@ -73,7 +79,7 @@
       }
       return (
         <div className="paper-form-container">
-          <form className="paper-form" onSubmit={this.handleSubmit}>
+          <form className="paper-form" onSubmit={this.handleNewPaper}>
             <label>View Markdown<input id="chk-markdown" type="checkbox" value="markdown" onChange={this.handleViewChange} /></label>
             { editorView }
           </form>
@@ -83,55 +89,86 @@
   });
 
   var Paper = React.createClass({
+    onClick: function(e) {
+      if (this.props.onClick) {
+        this.props.onClick(e);
+      }
+    },
+
     render: function() {
       return (
-        <div className="paper">
+        <div className="paper" onClick={this.onClick}>
           <h2 className="paper-title">Untitled paper - {this.props.paper.id}</h2>
+          <sub>{this.props.paper.dateUpdated.toISOString()}</sub>
         </div>
       );
     }
   });
 
   var PaperList = React.createClass({
+    handlePaperOnClick: function(props, paper, idx) {
+      if (typeof props.onSelectedPaper === 'function') {
+        props.onSelectedPaper(paper, idx);
+      }
+    },
+
     render: function() {
-      var paperNodes = this.props.data.map(function(paper) {
+      var _this = this;
+      var paperNodes = this.props.papers.map(function(paper, idx) {
         return (
-          <Paper paper={paper} key={paper.id} />
+          <Paper paper={paper} key={idx} onClick={_this.handlePaperOnClick.bind(_this, _this.props, paper, idx)} />
         );
       });
       return (
-        <div className="paperList">{paperNodes}</div>
+        <div className="paper-list">{paperNodes}</div>
       );
     }
   });
 
   var PaperBox = React.createClass({
-    loadPapersFromAPI: function() {
-      return [];
-    },
-
     getInitialState: function() {
+      // TODO make the asynchronous code to fetch papers from the API
       return { 
-        data: [] 
+        papers: [buildPaperModel()],
+        selectedPaperIdx: 0
       };
     },
 
-    componentDidMount: function() {
-      this.loadPapersFromAPI();
+    componentDidMount: function() {},
+
+    handleOnSelectedPaper: function(paper, idx, previousPaper) {
+      console.log('onSelectedPaper', paper, idx, previousPaper);
+      setState(this, {
+        selectedPaperIdx: idx
+      });
     },
 
     handleOnPaperSubmit: function(paper) {
-      this.setState({data: this.state.data.concat([paper])});
+      // Make sure to edit the proper paper
+      // For now we do not reorder the papers in any way
+      var papers = this.state.papers;
+      papers[this.state.selectedPaperIdx] = Object.assign({}, paper);
+      setState(this, {papers: papers});
         // TODO send to the server and update the data[]
       console.log(paper);
     },
 
+    handleOnCreateNewPaper: function() {
+      setState(this, {
+        papers: this.state.papers.concat([buildPaperModel()]),
+        selectedPaperIdx: this.state.papers.length
+      });
+    },
+
     render: function() {
+      console.log(Object.assign({}, this.state));
       return (
         <div className="paper-box">
           <h1>Papers</h1>
-          <PaperList data={this.state.data} />
-          <PaperForm onPaperSubmit={this.handleOnPaperSubmit} />
+          <PaperList papers={this.state.papers} onSelectedPaper={this.handleOnSelectedPaper}/>
+          <PaperForm paper={this.state.papers[this.state.selectedPaperIdx]} 
+            onPaperSubmit={this.handleOnPaperSubmit}
+            onCreateNewPaper={this.handleOnCreateNewPaper} />
         </div>
       );
     }
